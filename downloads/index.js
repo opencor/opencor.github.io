77e7adbd1a736a8f2e165a3908c5b5182ec2f841
agarny <agarny@users.noreply.github.com>
2021-10-05 06:33:44 +0000

    deploy: opencor/opencor@02673b68ffa7ebfb9a4a51d6bce679023dfcd5e4

---
+++ downloads/index.js
@@ -0,0 +1,207 @@
+var jsonData = { "versions": [
+                   { "major": 0, "minor": 6, "patch": 0, "day": 11, "month": 12, "year": 2018, "type": 1, "license": 2,
+                     "platforms": [
+                       { "name": "Windows", "supported": "Windows 7 and later",
+                         "files": [
+                           { "extension": ".exe" }, { "extension": ".zip" }
+                         ]
+                       },
+                       { "name": "Linux", "supported": "Ubuntu 16.04 LTS (Xenial Xerus) and later",
+                         "files": [
+                           { "extension": ".tar.gz" }
+                         ]
+                       },
+                       { "name": "macOS", "supported": "OS X 10.10 (Yosemite) and later",
+                         "files": [
+                           { "extension": ".dmg" }, { "extension": ".zip" }
+                         ]
+                       }
+                     ]
+                   },
+                   { "major": 0, "minor": 0, "patch": 0, "day": 5, "month": 10, "year": 2021, "type": 2, "license": 2,
+                     "platforms": [
+                       { "name": "Windows", "supported": "Windows 10",
+                         "files": [
+                           { "extension": ".exe" }, { "extension": ".zip" }
+                         ]
+                       },
+                       { "name": "Linux", "supported": "Ubuntu 20.04 LTS (Focal Fossa) and later",
+                         "files": [
+                           { "extension": ".tar.gz" }
+                         ]
+                       },
+                       { "name": "macOS", "supported": "macOS 10.12 (Sierra) and later",
+                         "files": [
+                           { "extension": ".pkg" }, { "extension": ".zip" }
+                         ]
+                       }
+                     ],
+                     "changes": [
+                      { "change": "<strong>CellML tools:</strong> suggest a better file name when exporting a remote <a href=\"https://cellml.org/\">CellML</a> 1.1 file to <a href=\"https://cellml.org/\">CellML</a> 1.0 (see issue <a href=\"https://github.com/opencor/opencor/issues/2579\">#2579</a>). Fixed an id-related issue when exporting a <a href=\"https://cellml.org/\">CellML</a> 1.1 file to <a href=\"https://cellml.org/\">CellML</a> 1.0 (see issue <a href=\"https://github.com/opencor/opencor/issues/2581\">#2581</a>)." },
+                      { "change": "<strong>CellML Text view:</strong> fixed some id-related issues when converting a <a href=\"https://cellml.org/\">CellML</a> file to the <a href=\"https://opencor.ws/user/plugins/editing/cellmlTextView.html#plugins-editing-cellmltextview-cellmltextformat\">CellML Text format</a> (see issue <a href=\"https://github.com/opencor/opencor/issues/575\">#575</a>)." },
+                      { "change": "<strong>Third-party libraries:</strong> upgraded <a href=\"https://katex.org/\">KaTeX</a> to version 0.13.18 (see issue <a href=\"https://github.com/opencor/opencor/issues/2583\">#2583</a>). Upgraded <a href=\"https://libgit2.github.com/\">libgit2</a> to version 1.3.0 (see issue <a href=\"https://github.com/opencor/opencor/issues/2584\">#2584</a>). Upgraded <a href=\"https://mesa3d.org/\">Mesa</a> to version 21.2.3 (see issue <a href=\"https://github.com/opencor/opencor/issues/2585\">#2585</a>). Upgraded <a href=\"https://computation.llnl.gov/projects/sundials\">SUNDIALS</a> to version 5.8.0 (see issue <a href=\"https://github.com/opencor/opencor/issues/2586\">#2586</a>). Upgraded <a href=\"https://packages.ubuntu.com/impish/oxygen-icon-theme\">Oxygen</a> to version 5.86.0 (see issue <a href=\"https://github.com/opencor/opencor/issues/2588\">#2588</a>)." }
+                     ]
+                   },
+                   { "major": 0, "minor": 5, "patch": 0, "day": 15, "month": 10, "year": 2016, "type": 0, "license": 1,
+                     "platforms": [
+                       { "name": "Windows", "supported": "Windows 7 and later",
+                         "files": [
+                           { "extension": ".exe" }, { "extension": ".zip" }
+                         ]
+                       },
+                       { "name": "Linux", "supported": "Ubuntu 14.04 LTS (Trusty Tahr) and later",
+                         "files": [
+                           { "extension": ".tar.gz" }
+                         ]
+                       },
+                       { "name": "macOS", "supported": "OS X 10.8 (Mountain Lion) and later",
+                         "files": [
+                           { "extension": ".dmg" }, { "extension": ".zip" }
+                         ]
+                       }
+                     ]
+                   },
+                   { "major": 0, "minor": 4, "patch": 1, "day": 20, "month": 5, "year": 2015, "type": 0, "license": 1,
+                     "platforms": [
+                       { "name": "Windows", "supported": "Windows 7 and later",
+                         "files": [
+                           { "extension": ".exe", "bitness": 32 }, { "extension": ".zip", "bitness": 32 },
+                           { "extension": ".exe", "bitness": 64 }, { "extension": ".zip", "bitness": 64 }
+                         ]
+                       },
+                       { "name": "Linux", "supported": "Ubuntu 14.04 LTS (Trusty Tahr) and later",
+                         "files": [
+                           { "extension": ".tar.gz", "bitness": 32 },
+                           { "extension": ".tar.gz", "bitness": 64 }
+                         ]
+                       },
+                       { "name": "macOS", "supported": "Mac OS X 10.7 (Lion) and later",
+                         "files": [
+                           { "extension": ".dmg" }, { "extension": ".zip" }
+                         ]
+                       }
+                     ]
+                   },
+                   { "major": 0, "minor": 4, "patch": 0, "day": 29, "month": 4, "year": 2015, "type": 0, "license": 1,
+                     "platforms": [
+                       { "name": "Windows", "supported": "Windows 7 and later",
+                         "files": [
+                           { "extension": ".exe", "bitness": 32 }, { "extension": ".zip", "bitness": 32 },
+                           { "extension": ".exe", "bitness": 64 }, { "extension": ".zip", "bitness": 64 }
+                         ]
+                       },
+                       { "name": "Linux", "supported": "Ubuntu 14.04 LTS (Trusty Tahr) and later",
+                         "files": [
+                           { "extension": ".tar.gz", "bitness": 32 },
+                           { "extension": ".tar.gz", "bitness": 64 }
+                         ]
+                       },
+                       { "name": "macOS", "supported": "Mac OS X 10.7 (Lion) and later",
+                         "files": [
+                           { "extension": ".dmg" }, { "extension": ".zip" }
+                         ]
+                       }
+                     ]
+                   },
+                   { "major": 0, "minor": 3, "patch": 0, "day": 19, "month": 8, "year": 2014, "type": 0, "license": 1,
+                     "platforms": [
+                       { "name": "Windows", "supported": "Windows XP and later",
+                         "files": [
+                           { "extension": ".exe" }, { "extension": ".zip" }
+                         ]
+                       },
+                       { "name": "Linux", "supported": "Ubuntu 14.04 LTS (Trusty Tahr) and later",
+                         "files": [
+                           { "extension": ".tar.gz", "bitness": 32 },
+                           { "extension": ".tar.gz", "bitness": 64 }
+                         ]
+                       },
+                       { "name": "macOS", "supported": "Mac OS X 10.7 (Lion) and later",
+                         "files": [
+                           { "extension": ".dmg" }, { "extension": ".zip" }
+                         ]
+                       }
+                     ]
+                   },
+                   { "major": 0, "minor": 2, "patch": 0, "day": 19, "month": 11, "year": 2013, "type": 0, "license": 1,
+                     "platforms": [
+                       { "name": "Windows", "supported": "Windows XP and later",
+                         "files": [
+                           { "extension": ".exe" }, { "extension": ".zip" }
+                         ]
+                       },
+                       { "name": "Linux", "supported": "Ubuntu 12.04 LTS (Precise Pangolin) and later",
+                         "files": [
+                           { "extension": ".tar.gz", "bitness": 32 },
+                           { "extension": ".tar.gz", "bitness": 64 }
+                         ]
+                       },
+                       { "name": "macOS", "supported": "Mac OS X 10.7 (Lion) and later",
+                         "files": [
+                           { "extension": ".dmg" }, { "extension": ".zip" }
+                         ]
+                       }
+                     ]
+                   },
+                   { "major": 0, "minor": 1, "patch": 2, "day": 29, "month": 5, "year": 2013, "type": 0, "license": 0,
+                     "platforms": [
+                       { "name": "Windows", "supported": "Windows XP and later",
+                         "files": [
+                           { "extension": ".exe" }, { "extension": ".zip" }
+                         ]
+                       },
+                       { "name": "Linux", "supported": "Ubuntu 12.04 LTS (Precise Pangolin) and later",
+                         "files": [
+                           { "extension": ".tar.gz", "bitness": 32 },
+                           { "extension": ".tar.gz", "bitness": 64 }
+                         ]
+                       },
+                       { "name": "macOS", "supported": "OS X 10.8 (Mountain Lion) and later",
+                         "files": [
+                           { "extension": ".dmg" }, { "extension": ".zip" }
+                         ]
+                       }
+                     ]
+                   },
+                   { "major": 0, "minor": 1, "patch": 1, "day": 17, "month": 4, "year": 2013, "type": 0, "license": 0,
+                     "platforms": [
+                       { "name": "Windows", "supported": "Windows XP and later",
+                         "files": [
+                           { "extension": ".exe" }, { "extension": ".zip" }
+                         ]
+                       },
+                       { "name": "Linux", "supported": "Ubuntu 12.04 LTS (Precise Pangolin) and later",
+                         "files": [
+                           { "extension": ".tar.gz", "bitness": 32 },
+                           { "extension": ".tar.gz", "bitness": 64 }
+                         ]
+                       },
+                       { "name": "macOS", "supported": "OS X 10.8 (Mountain Lion) and later",
+                         "files": [
+                           { "extension": ".dmg" }, { "extension": ".zip" }
+                         ]
+                       }
+                     ]
+                   },
+                   { "major": 0, "minor": 1, "patch": 0, "day": 1, "month": 4, "year": 2013, "type": 0, "license": 0,
+                     "platforms": [
+                       { "name": "Windows", "supported": "Windows XP and later",
+                         "files": [
+                           { "extension": ".exe" }, { "extension": ".zip" }
+                         ]
+                       },
+                       { "name": "Linux", "supported": "Ubuntu 12.04 LTS (Precise Pangolin) and later",
+                         "files": [
+                           { "extension": ".tar.gz", "bitness": 32 },
+                           { "extension": ".tar.gz", "bitness": 64 }
+                         ]
+                       },
+                       { "name": "macOS", "supported": "OS X 10.8 (Mountain Lion) and later",
+                         "files": [
+                           { "extension": ".dmg" }, { "extension": ".zip" }
+                         ]
+                       }
+                     ]
+                   }
+                 ]
+               };
