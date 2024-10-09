8b2542222e7b60cbcfe3afd66fa100af4ff21a94
agarny <agarny@users.noreply.github.com>
2024-10-09 20:41:06 +0000

    deploy: opencor/general-documentation@97efc2103625fbde7793df02a5a5a5bb818540db

--- whatIsNew.js
+++ whatIsNew.js
@@ -1,5 +1,5 @@
 var jsonData = { "versions": [
-                   { "anchor": "latest", "description": "Latest snapshot", "day": 23, "month": 8, "year": 2024,
+                   { "anchor": "latest", "description": "Latest snapshot", "day": 10, "month": 10, "year": 2024,
                      "categories": [
                        { "name": "Data store",
                          "entries": [
